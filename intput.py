6fd0e4f6c716be3052ffe03c7802db04647d0c71
Danielooo <dvanraffe@gmail.com>
2024-08-28 14:21:00 +0200

    added input.py assignments

---
+++ intput.py
@@ -0,0 +1,23 @@
+# ==========================================
+# Voorbeeld Opdracht
+# Voer je naam in met de input() methode en print daarna je naam in de console.
+# ==========================================
+
+naam = input('Voer je naam in: ')  # voorbeeld invoer: Bart
+print('Je naam is: ', naam)  # Het resultaat is: Je naam is: Bart
+
+
+# ==========================================
+# Opgave 1:
+# Gegeven is het woord 'Python'. Schrijf een programma dat de gebruiker vraagt om input. Als de gebruiker het woord 'Python' invoert, print dan de boolean True, anders print False.
+# ==========================================
+
+
+
+# ==========================================
+# Opgave 2:
+# Schrijf een programma dat de gebruiker vraagt om een getal. Voer daarna nog een getal in en print de som van de twee getallen.
+
+# Verwachte uitkomst bij invoer van getallen 2 en 3:  De som van 2 en 3 is : 5
+# ==========================================
+
